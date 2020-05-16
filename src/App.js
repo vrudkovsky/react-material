0d4be3f452f42cba51db7d72de9c015fe7a51e97
Victor Rudkovsky <vrudkovsky@vitasoftua.com>
2020-05-17 00:43:51 +0300

    drawer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AppHeader from "./components/Header/Header";
+import Drawer from "./components/Drawer/Drawer";
 
 
 export default function App() {
     return (
         <div>
             <AppHeader />
+            <Drawer />
         </div>
     );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,6 +6,23 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Drawer from "@material-ui/core/Drawer";
+import clsx from "clsx";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
+import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -23,29 +40,164 @@ const useStyles = makeStyles((theme) => ({
 
 }));
 
+
+
 export default function AppHeader() {
     const classes = useStyles();
+    const [auth, setAuth] = React.useState(true);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+
+    const [state, setState] = React.useState({
+        top: false,
+        left: false,
+        bottom: false,
+        right: false,
+    });
+
+    const handleChange = (event) => {
+        setAuth(event.target.checked);
+    };
+
+    const handleMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const toggleDrawer = (anchor, open) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+
+        setState({ ...state, [anchor]: open });
+    };
+
+
+    const list = (anchor) => (
+        <div
+            className={clsx(classes.list, {
+                [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+            })}
+            role="presentation"
+            onClick={toggleDrawer(anchor, false)}
+            onKeyDown={toggleDrawer(anchor, false)}
+        >
+            <List>
+                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+            <Divider />
+            <List>
+                {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+            <Divider />
+            <List>
+                {['Zalupa', 'Hui', 'Pizda'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+        </div>
+    );
 
     return (
         <div className={classes.root}>
+            <FormGroup>
+                <FormControlLabel
+                    control={<Switch checked={auth} onChange={handleChange} aria-label="login switch" />}
+                    label={auth ? 'Logout' : 'Login'}
+                />
+            </FormGroup>
             <AppBar
                 position="static"
-                color="default"
-
+                style={{ background: 'indigo700' }}
             >
+
                 <Toolbar>
                     <Typography variant="h6" className={classes.title}>
-                        News
+                        News1
                     </Typography>
                     <Typography variant="h6" className={classes.title}>
-                        News
+                        News2
                     </Typography>
                     <Button color="inherit">Login</Button>
-                    <IconButton edge="end" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
+
+                    {auth && (
+                        <div>
+                            <IconButton
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleMenu}
+                                color="inherit"
+                            >
+                                <AccountCircle />
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorEl}
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                open={open}
+                                onClose={handleClose}
+                            >
+                                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                                <MenuItem onClick={handleClose}>My account</MenuItem>
+                            </Menu>
+                        </div>
+                    )}
+                    {['right'].map((anchor) => (
+                        <IconButton edge="end" className={classes.menuButton} color="inherit" aria-label="menu" >
+                            <MenuIcon onClick={toggleDrawer(anchor, true)} />
+                            <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
+                                {list(anchor)}
+                            </Drawer>
+                        </IconButton>
+                    ))}
                 </Toolbar>
             </AppBar>
         </div>
     );
 }
+
+/*
+{['right'].map((anchor) => (
+    <IconButton edge="end" className={classes.menuButton} color="inherit" aria-label="menu" >
+        <MenuIcon onClick={toggleDrawer(anchor, true)} />
+        <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
+            {list(anchor)}
+        </Drawer>
+    </IconButton>
+))}
+
+
+{['left', 'right', 'top', 'bottom'].map((anchor) => (
+    <React.Fragment key={anchor}>
+        <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+        <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
+            {list(anchor)}
+        </Drawer>
+    </React.Fragment>
+))}
+*/
